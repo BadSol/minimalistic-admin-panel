ee3260f4474d79cc6128d2dbc7eabf09c9067a33
Sol <rygula.dawid@gmail.com>
2016-11-02 23:18:55 +0100

    model_view now displays list of all existing objects for given model, added very crude template to display objects.

--- min_admin/views.py
+++ min_admin/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.apps import apps
 from django.http import Http404
+from models import Person, Teacher, Class
 
 
 def index_view(request):
@@ -19,7 +20,10 @@ def model_view(request, model_name):
     if not check_if_model_exist(model_name):
         raise Http404("\"{}\" model doesn't exist".format(model_name))
 
-    return render(request, 'min_admin/model_objects.html', {'models': model_name})
+    model = apps.get_app_config('min_admin').get_model(model_name)
+    objects = model.objects.all()
+    context = {'objects': objects}
+    return render(request, 'min_admin/model_objects.html', context)
 
 
 def check_if_model_exist(model_name):
