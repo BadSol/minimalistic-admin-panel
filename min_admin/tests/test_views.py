1f0f3c8b9c26261d88effab35fcbd0d29f4e93fd
Sol <rygula.dawid@gmail.com>
2016-11-02 13:00:25 +0100

    Added first view tests

--- min_admin/tests/test_views.py
+++ min_admin/tests/test_views.py
@@ -1,5 +1,19 @@
-def test_pytest():
-    assert(2+2 == 4)
+from django.test import TestCase
+from django.urls import reverse
+from django.apps import apps
 
 
+class MinAdminApiViewTest(TestCase):
+    def test_index_view_status_code_200(self):
+        response = self.client.get(reverse('index'))
+        assert(response.status_code == 200)
+
+    def test_index_view_response_contain_model_names(self):
+        model_names = []
+        for model in apps.get_app_config('min_admin').get_models():
+            model_names.append(model._meta.verbose_name)
+
+        response = self.client.get(reverse('index'))
+
+        assert(all(model in response.content for model in model_names))
 
