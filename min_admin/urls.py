72d4ab6c65b5c1399d45a8c95c8a8141161f4c5f
Sol <rygula.dawid@gmail.com>
2016-11-03 20:19:21 +0100

    Added detail_view view + template which is basically model instance detail view, it's accessed via url "min_admin/<model name>/<object primary key>". Changed model_objects templates a bit, to use different data format (I am using model_to_dict and then .items() method).

--- min_admin/urls.py
+++ min_admin/urls.py
@@ -5,4 +5,5 @@ app_name = 'min_admin'
 urlpatterns = [
     url(r'^$', views.index_view, name='models'),
     url(r'^(?P<model_name>\w{0,20})/$', views.model_view, name='modelObjects'),  # accepting models with up to 20 chars
+    url(r'^(?P<model_name>\w{0,20})/(?P<pk>[0-9]+)/$', views.detail_view, name='objectDetail')
 ]
--- min_admin/views.py
+++ min_admin/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render
 from django.apps import apps
 from django.http import Http404
 from django.core.exceptions import ObjectDoesNotExist
-
+from django.forms.models import model_to_dict
+from django.core import serializers
 # Utils:
 
 
@@ -22,18 +23,19 @@ def crete_list_of_objects_with_attributes(objects, model_fields):
     """
     result = []
     for obj in objects:
-        foo = [obj]
-        for field in model_fields:
-            print "field {}".format(field)
-            try:
-                foo.append(getattr(obj, field.name))
-            except ObjectDoesNotExist:
-                foo.append(None)
-            except AttributeError:
-                foo.append(None)
-        result.append(foo)
+        result.append([obj, model_to_dict(obj).items()])
 
     return result
+
+
+def get_model_name_or_404(model_name):
+    """ returns model_name.lower() or raise 404 if model doesn't exist"""
+    model_name = model_name.lower()
+
+    if not check_if_model_exist(model_name):
+        raise Http404("\"{}\" model doesn't exist".format(model_name))
+    return model_name
+
 
 # VIEWS:
 
@@ -49,18 +51,29 @@ def index_view(request):
 
 
 def model_view(request, model_name):
-    model_name = model_name.lower()
-
-    if not check_if_model_exist(model_name):
-        raise Http404("\"{}\" model doesn't exist".format(model_name))
+    model_name = get_model_name_or_404(model_name)
 
     model = apps.get_app_config('min_admin').get_model(model_name)
     model_fields = model._meta.get_fields()
     objects = model.objects.all()
     context = {'objects': crete_list_of_objects_with_attributes(objects, model_fields),
-               'fields': model_fields}
+               'fields': model_fields,
+               'model_name': model_name}
 
     return render(request, 'min_admin/model_objects.html', context)
 
 
+def detail_view(request, model_name, pk):
+    model_name = get_model_name_or_404(model_name)
+    model = apps.get_app_config('min_admin').get_model(model_name)
+    try:
+        object_instance = model.objects.get(pk=pk)
+    except ObjectDoesNotExist:
+        raise Http404("\"{}\" object doesn't exist".format(model_name))
 
+    data = model_to_dict(object_instance)
+
+    context = {'object': object_instance,
+               'data': data.items(),
+               'model_name': model_name.capitalize()}
+    return render(request, 'min_admin/details.html', context)
